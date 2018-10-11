714ac6eae509cccf385ef941115fc3f5f9cb875e
Jon <gogs@fake.local>
2018-10-11 13:30:37 +0100

    Fixed clobbering of arg 4

--- src/guminterceptor-mips.c
+++ src/guminterceptor-mips.c
@@ -162,7 +162,7 @@ _gum_interceptor_backend_create_trampoline (GumInterceptorBackend * self,
   }
 
   /* TODO: save $t0 on the stack? */
-  gum_mips_writer_put_la_reg_address (cw, MIPS_REG_T0, GUM_ADDRESS (ctx));
+  gum_mips_writer_put_la_reg_address (cw, MIPS_REG_T4, GUM_ADDRESS (ctx));
   gum_mips_writer_put_la_reg_address (cw, MIPS_REG_AT,
       GUM_ADDRESS (self->enter_thunk->data));
   gum_mips_writer_put_jr_reg (cw, MIPS_REG_AT);
@@ -170,7 +170,7 @@ _gum_interceptor_backend_create_trampoline (GumInterceptorBackend * self,
   ctx->on_leave_trampoline = gum_mips_writer_cur (cw);
 
   /* TODO: save $t0 on the stack? */
-  gum_mips_writer_put_la_reg_address (cw, MIPS_REG_T0, GUM_ADDRESS (ctx));
+  gum_mips_writer_put_la_reg_address (cw, MIPS_REG_T4, GUM_ADDRESS (ctx));
   gum_mips_writer_put_la_reg_address (cw, MIPS_REG_AT,
       GUM_ADDRESS (self->leave_thunk->data));
   gum_mips_writer_put_jr_reg (cw, MIPS_REG_AT);
@@ -339,7 +339,7 @@ gum_emit_enter_thunk (GumMipsWriter * cw)
 
   gum_mips_writer_put_call_address_with_arguments (cw,
       GUM_ADDRESS (_gum_function_context_begin_invocation), 4,
-      GUM_ARG_REGISTER, MIPS_REG_T0,
+      GUM_ARG_REGISTER, MIPS_REG_T4,
       GUM_ARG_REGISTER, MIPS_REG_A1,  // CPU_CONTEXT
       GUM_ARG_REGISTER, MIPS_REG_A2,  // RA
       GUM_ARG_REGISTER, MIPS_REG_A3); // NEXT_HOP
@@ -359,7 +359,7 @@ gum_emit_leave_thunk (GumMipsWriter * cw)
 
   gum_mips_writer_put_call_address_with_arguments (cw,
       GUM_ADDRESS (_gum_function_context_end_invocation), 3,
-      GUM_ARG_REGISTER, MIPS_REG_T0,
+      GUM_ARG_REGISTER, MIPS_REG_T4,
       GUM_ARG_REGISTER, MIPS_REG_A1,  // CPU CONTEXT
       GUM_ARG_REGISTER, MIPS_REG_A2); // NEXT_HOP
 
